2411fb872aeafcf389b6c2ff38fcf64122431b0a
stevenxcao <steven.h.cao@gmail.com>
2018-05-02 23:31:03 -0700

    Added logging

--- train.py
+++ train.py
@@ -10,35 +10,47 @@ from transform import *
 
 from os import mkdir
 from os.path import exists, join
+import sys
 import time
+
+class Logger(object):
+    def __init__(self, filename="Default.log"):
+        self.terminal = sys.stdout
+        self.log = open(filename, "a")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
 
 def main():
     if torch.cuda.is_available():
-        torch.cuda.set_device(0)
+        torch.cuda.set_device(1)
 
-    num_epochs = 5
+    num_epochs = 3
     load = True
     t = transforms.Compose([MagPhase(), PickChannel(0),
-                            ToTensor()]) #Residual(), Decimate()
+                            Residual(), ToTensor()]) #Decimate()
     
 #    name = 'dncnn_mag_patch32'
 #    name = 'dncnn_phase_patch32'
 #    train = NdarrayDatasetPatch('../data-npy/train', transform = t)
 #    test = NdarrayDatasetPatch('../data-npy/test', transform = t)
-#    name = 'dncnn_mag_256'
-#    name += '_d40'
+    name = 'dncnn_mag_256'
+    name += '_d40'
 #    name += '_l1'
 #    name += '_smooth'
 #    name = 'dncnn_phase_256'
 #    name = 'dncnn_mag_128'
 #    name = 'dncnn_real_128'
-    name = 'dncnn_nores_mag_256'
+#    name = 'dncnn_nores_mag_256'
     train = NdarrayDataset2d('../data-npy/train', transform = t)
     test = NdarrayDataset2d('../data-npy/test', transform = t)
     
     criterion = torch.nn.MSELoss() #(Smooth)L1Loss(), MSELoss()
-    depth = 20
+    depth = 40
     #####
+    sys.stdout = Logger(join(name, "log.txt"))
+    
     example = train[0]['image'] # C x H x W
     in_size = example.shape[1:]
     in_ch = example.shape[0]
@@ -95,7 +107,7 @@ def main():
                     % (test_loss, time.time() - total_time)))
                 train_loss = 0.0
             if i % display_every_i == 0:
-                print('[%d] Training loss: %.3f, Time: %3f' 
+                print('[%d] Training loss: %.3f, Time: %.3f' 
                     % (i + 1, train_loss, time.time() - total_time))
         torch.save(net.state_dict(), join(name, 'model.pth'))
         np.save(join(name, 'losses.npy'), losses)
